8bf032abdeaa4476c260e3d2495452add8a8e82c
Mike <cassaro.mike@gmail.com>
2015-10-04 11:13:59 -0700

    Updating methods & views movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
 
-#Index method to show all the instances of movies. I want this to pull form Enceladus.
+#Index method to show all instances of popular movies.
   def index
     @movies = $popular_movies.all
   end
@@ -19,12 +19,4 @@ $popular_movies = Enceladus::Movie.popular
 #$popular_movies.all.each do |movie|
   #movie.title
      #end
-
-  private
-
-#WCaptures movie attributes via a form and sets them to the controller. Eventually won't permit title and year as they will be passed it and users will not be able to change.
-  def movie_params
-    params.require(:movie)
-  end
-
 end
